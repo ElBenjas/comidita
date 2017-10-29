3822bb4c7b9b190bd65be7530096ebd3141e58b6
Benjamín Peña <benjamin.pena.olvera@gmail.com>
2017-10-29 13:33:41 -0600

    Show action users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+  test 'GET #index' do
+    get users_url
+
+    assert_response :success
+  end
+
+  test 'GET #show' do
+    get users_url{user.id}
+
+    assert_response :success
+  end
 end
