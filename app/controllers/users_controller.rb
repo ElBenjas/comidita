b75dc42ff3783f9249d038574929fdfb39fd7868
Benjamín Peña <benjamin.pena.olvera@gmail.com>
2017-10-31 17:35:10 -0600

    CRUD for users complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
+  # protect_from_forgery with: :null_session
+
   def index
     users = User.all
     render json: users
   end
 
+  def create
+    user = User.new(user_params)
+    user.save
+
+    render json: user
+  end
+
   def show
     user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+
     render json: user
   end
 
@@ -14,4 +30,10 @@ class UsersController < ApplicationController
     user.destroy
     render json: user
   end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password)
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,19 +1,54 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
   setup do
     @user = users(:one)
   end
+
   test 'GET #index' do
     get users_url
 
     assert_response :success
   end
 
+  test 'POST #create' do
+    params = {
+      user: {
+        name: 'Test',
+        email: 'test@testing.com',
+        password: ''
+      }
+    }
+
+    assert_difference 'User.count', 1 do
+      post users_url,
+      params: params
+
+      assert_response :success
+    end
+  end
+
   test 'GET #show' do
     get users_url(id: @user.id)
 
     assert_response :success
+  end
+
+  test 'PUT #update' do
+    params = {
+      user: {
+        name: 'Test'
+      }
+    }
+
+    assert_changes '@user.name' do
+      put user_url(id: @user.id),
+      params: params
+
+      assert_response :success
+      @user.reload
+    end
   end
 
   test 'DELETE #destroy' do
