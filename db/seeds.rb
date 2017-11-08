d6c009f14739f43f708e7daf2fe2c6d9a6d6666e
Benjamín Peña <benjamin.pena.olvera@gmail.com>
2017-11-08 14:24:44 -0600

    no comments in Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.destroy_all
 Picture.destroy_all
 
@@ -73,16 +65,7 @@ User.create!([
 
   p "Created #{User.count} users."
 
-
-  #
-  # create_table "users", force: :cascade do |t|
-  #   t.string "name"
-  #   t.string "emaill"
-  #   t.string "password"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-inicio = User.first.id
+start = User.first.id
 final = User.last.id
 
 captions = ['Yummi', 'Delicioso', 'Ya comieron, pobres?', 'Rico']
@@ -90,15 +73,5 @@ total_captions = captions.count
 
 50.times do
   captions.shuffle!
-  Picture.create(pic: "http://lorempixel.com/1200/800/food/", caption: "#{captions[total_captions - 1]}", user_id: rand(inicio..final))
+  Picture.create(pic: "http://lorempixel.com/1200/800/food/", caption: "#{captions[total_captions - 1]}", user_id: rand(start..final))
 end
-
-#
-# create_table "pictures", force: :cascade do |t|
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-#   t.string "pic"
-#   t.string "caption"
-#   t.bigint "user_id"
-#   t.index ["user_id"], name: "index_pictures_on_user_id"
-# end
